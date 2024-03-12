b442d3ad143ab73f1214e4d33e9a4b864d7e493b
MrFrangipane <contact@frangipane.me>
2024-03-12 11:33:12 +0100

    fix(MIDI): fallback to fixed bpm when no midi port found

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 NAME = 'oscartnetdaemon'
-VERSION = '0.0.0'
+VERSION = '0.0.1'
 DESCRIPTION = 'Animate and control lighting fixtures with OSC and Artnet'
 AUTHOR = 'Frangitron'
 AUTHOR_EMAIL = 'contact@frangitron.com'
