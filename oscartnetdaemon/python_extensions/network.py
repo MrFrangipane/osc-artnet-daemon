d60d65a4cf09280000b880f002ebf19a784ced18
MrFrangipane <contact@frangipane.me>
2024-07-01 19:16:33 +0200

    fix(Domain:Network): remove ping stdout print

--- oscartnetdaemon/python_extensions/network.py
+++ oscartnetdaemon/python_extensions/network.py
@@ -33,4 +33,4 @@ def ping(host):
     """
     param = '-n' if platform.system().lower() == 'windows' else '-c'
     command = ['ping', param, '1', host]
-    return subprocess.call(command) == 0
+    return subprocess.call(command, stdout=subprocess.DEVNULL) == 0
