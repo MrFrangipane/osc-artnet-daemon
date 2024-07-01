df48879a3f3d6958683d474bad620982458186a6
MrFrangipane <contact@frangipane.me>
2024-07-01 10:17:21 +0200

    feat(SimpleDMXConsole): Begin Fixture repository

--- examples/simple-dmx-console/simpledmxconsole/fixture/base_definition.py
+++ examples/simple-dmx-console/simpledmxconsole/fixture/base_definition.py
@@ -1,5 +1,24 @@
-from simpledmxconsole.fixture.dmx_channel import DMXChannel
+from copy import copy
+
+from simpledmxconsole.fixture.dmx_channel import BaseDMXChannel, DMXChannel, DMXChannelFloat
 
 
 class BaseFixtureDefinition:
-    Channels: list[DMXChannel] = list()
+    Channels: list[BaseDMXChannel] = list()
+
+    def __init__(self):
+        self.channels: list[BaseDMXChannel] = list()
+
+    def create_channels(self, start_channel: int):
+        channel_number = start_channel
+        self.channels = list()
+
+        for channel in self.Channels:
+            new_channel = copy(channel)
+            new_channel.channel_number = channel_number
+            self.channels.append(new_channel)
+
+            if isinstance(channel, DMXChannelFloat):
+                channel_number += 2
+            else:
+                channel_number += 1
