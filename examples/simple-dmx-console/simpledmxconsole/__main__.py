f8e245530d1c8b29f33d2d17f7fd0cb68ec91adb
MrFrangipane <contact@frangipane.me>
2024-07-04 11:59:16 +0200

    feat(SDC): ChangeNotificationScope (Brodcast, Local, Foreign) + begin OSC aquivalent of MIDI

--- examples/simple-dmx-console/simpledmxconsole/__main__.py
+++ examples/simple-dmx-console/simpledmxconsole/__main__.py
@@ -1,8 +1,13 @@
+import logging
+
 from oscartnetdaemon.components.midi.service_registerer import MIDIServiceRegisterer
 from oscartnetdaemon.components.osc.service_registerer import OSCServiceRegisterer
 from oscartnetdaemon.components.service_repository import ServiceRepository
 
 from simpledmxconsole.artnet.service_registerer import ArtnetServiceRegisterer
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == '__main__':
