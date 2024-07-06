52d332e01968fb7543ef55f7a25f8aa16058b697
MrFrangipane <contact@frangipane.me>
2024-07-06 17:30:18 +0200

    update requirements.txt to point to fork of mido + remove OSC to check if raspberry is faster without it

--- examples/advanced-dmx-console/advanceddmxconsole/__main__.py
+++ examples/advanced-dmx-console/advanceddmxconsole/__main__.py
@@ -1,7 +1,6 @@
 import logging
 
 from oscartnetdaemon.components.midi.service_registerer import MIDIServiceRegisterer
-from oscartnetdaemon.components.osc.service_registerer import OSCServiceRegisterer
 from oscartnetdaemon.components.service_repository import ServiceRepository
 
 from advanceddmxconsole.service_registerer import ArtnetServiceRegisterer
@@ -14,7 +13,6 @@ if __name__ == '__main__':
     service_repository = ServiceRepository()
 
     service_repository.register(MIDIServiceRegisterer)
-    service_repository.register(OSCServiceRegisterer)
 
     # Register last to ensure Variable initialization will happen last
     service_repository.register(ArtnetServiceRegisterer)
