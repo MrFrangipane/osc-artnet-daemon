1b49b616e67fb9887e52e92d3f55258909ad633b
MrFrangipane <contact@frangipane.me>
2025-01-07 20:24:38 +0100

    feat: Pattern Editor

--- oscartnetdaemon/components/pattern_store/api.py
+++ oscartnetdaemon/components/pattern_store/api.py
@@ -4,5 +4,13 @@ from oscartnetdaemon.core.components import Components
 class PatternStoreAPI:
 
     @staticmethod
-    def get_step(fixture_type: str, group_place: int) -> dict[str, int]:
-        return Components().pattern_store.get_step(fixture_type, group_place)
+    def get_step_while_playing(fixture_type: str, group_place: int) -> dict[str, int]:
+        return Components().pattern_store.get_step_while_playing(fixture_type, group_place)
+
+    @staticmethod
+    def get_pattern(fixture_type: str, group_place: int, pattern_index: int) -> list[dict[str, int]]:
+        return Components().pattern_store.get_pattern(fixture_type, group_place, pattern_index)
+
+    @staticmethod
+    def set_pattern(fixture_type: str, group_place: int, pattern_index: int, pattern: list[dict[str, int]]):
+        Components().pattern_store.set_pattern(fixture_type, group_place, pattern_index, pattern)
--- oscartnetdaemon/components/pattern_store/store.py
+++ oscartnetdaemon/components/pattern_store/store.py
@@ -6,7 +6,7 @@ class PatternStore:
     def __init__(self):
         self.data: dict[str, list[list[list[dict[str, int]]]]] = dict()
 
-    def get_step(self, fixture_type: str, group_place: int) -> dict[str, int]:
+    def get_step_while_playing(self, fixture_type: str, group_place: int) -> dict[str, int]:
         beat_counter = Components().midi_tempo.beat_counter
         mood = Components().osc_state_model.mood
         beat = beat_counter * [.25, .5, 1.0, 2.0, 4.0][mood.bpm_scale]
@@ -17,3 +17,12 @@ class PatternStore:
             return {}
 
         return pattern[int(beat) % len(pattern)]
+
+    def get_pattern(self, fixture_type: str, group_place: int, pattern_index: int) -> list[dict[str, int]]:
+        if fixture_type not in self.data:
+            return []
+
+        return self.data[fixture_type][pattern_index][group_place]
+
+    def set_pattern(self, fixture_type: str, group_place: int, pattern_index: int, pattern: list[dict[str, int]]):
+        self.data[fixture_type][pattern_index][group_place] = pattern
