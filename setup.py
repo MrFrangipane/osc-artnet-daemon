8c1d8c816c712df012b9363ca0a6cf72d88f959a
MrFrangipane <contact@frangipane.me>
2025-03-15 14:11:02 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 NAME = 'oscartnetdaemon'
-VERSION = '0.2.1'
+VERSION = '0.3.0'
 DESCRIPTION = 'Animate and control lighting fixtures with OSC and Artnet'
 AUTHOR = 'Frangitron'
 AUTHOR_EMAIL = 'contact@frangitron.com'
